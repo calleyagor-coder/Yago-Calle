95fd5895e59e518be50dcc4507c1c3de9b2f5ff9
Yago <calleyagor@gmail.com>
2026-03-19 17:35:36 -0300

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,21 @@
+package ejerciciosderepaso;
+
+import java.util.Scanner;
+/**
+ *
+ * @author LABO B 11
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+
+        String nombre1="Leonel";
+        String nombre2="Andres";
+        String apellido="Messi";
+     
+        
+        System.out.println("Generando email..........");
+        System.out.println(nombre1+"_"+nombre2+"_"+apellido+"@CEN.TECH.COM.AR");
+ 
+    }
+}
