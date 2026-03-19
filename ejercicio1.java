5668fa7d32e495b70484f7e7baca9e21441ca7db
Yago <calleyagor@gmail.com>
2026-03-19 17:26:12 -0300

    Practica-Ejercicios de repaso

---
+++ ejercicio1.java
@@ -0,0 +1,25 @@
+package ejerciciosderepaso;
+
+import java.util.Scanner;
+/**
+ *
+ * @author LABO B 11
+ */
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        
+        String nombrecompleto="Yago Calle";
+        int edad=17;
+        double altura=1.70;
+        String paisdeorigen="Argentina";
+        char estadocivil='S';
+        
+        System.out.println("Nombre Completo: "+nombrecompleto);
+        System.out.println("Edad: "+edad);
+        System.out.println("Altura: "+altura);
+        System.out.println("Pais de Origen "+paisdeorigen);
+        System.out.println("Estado Civil: "+estadocivil);
+        
+    }
+}
